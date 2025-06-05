94248d524db847195768a6822b2283e4c67ca0f0
문석현 <munseoghyeon@MacBook-Pro.local>
2025-06-05 17:10:40 +0900

    s

--- app/Controllers/SitemapController.php
+++ app/Controllers/SitemapController.php
@@ -11,7 +11,6 @@ class SitemapController extends Controller
     {
         $sitemapModel = new SitemapModel();
 
-        // 데이터 총 개수
         $totalEvents               = $sitemapModel->countAllEvents();
         $totalGasStations          = $sitemapModel->countAllGasStations();
         $totalParkingLots          = $sitemapModel->countAllParkingLots();
@@ -19,228 +18,94 @@ class SitemapController extends Controller
         $totalRepairShops          = $sitemapModel->countAllRepairShops();
         $totalCarWashes            = $sitemapModel->countAllCarWashes();
         $totalTowedVehicleStorages = $sitemapModel->countAllTowedVehicleStorages();
-        $totalParkingFacilities    = $sitemapModel->countAllParkingFacilities(); // 공영주차장
+        $totalParkingFacilities    = $sitemapModel->countAllParkingFacilities();
         $totalStores               = $sitemapModel->countAllStores();
-        $totalEvStations           = $sitemapModel->countAllEvStations(); // 전기차 충전소
-        $totalChargingStations     = $sitemapModel->countAllChargingStations(); // 추가된 charging_stations
+        $totalEvStations           = $sitemapModel->countAllEvStations();
+        $totalChargingStations     = $sitemapModel->countAllChargingStations();
 
         $itemsPerPage = 10000;
 
-        // 페이지 수 계산
-        $eventPages               = ceil($totalEvents               / $itemsPerPage);
-        $gasStationPages          = ceil($totalGasStations          / $itemsPerPage);
-        $parkingLotPages          = ceil($totalParkingLots          / $itemsPerPage);
-        $hotelPages               = ceil($totalHotels               / $itemsPerPage);
-        $repairShopPages          = ceil($totalRepairShops          / $itemsPerPage);
-        $carWashPages             = ceil($totalCarWashes             / $itemsPerPage);
-        $towedVehicleStoragePages = ceil($totalTowedVehicleStorages / $itemsPerPage);
-        $parkingFacilityPages     = ceil($totalParkingFacilities     / $itemsPerPage); // 공영주차장
-        $storePages               = ceil($totalStores               / $itemsPerPage);
-        $evStationPages           = ceil($totalEvStations           / $itemsPerPage); // 전기차 충전소
-        $chargingStationPages     = ceil($totalChargingStations     / $itemsPerPage); // charging_stations
-
-        // XML 시작
         $xml  = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
         $xml .= "<sitemapindex xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\">\n";
 
-        $xml .= $this->addSitemapEntries('events',            $eventPages);
-        $xml .= $this->addSitemapEntries('gasstations',       $gasStationPages);
-        $xml .= $this->addSitemapEntries('parkinglots',       $parkingLotPages);
-        $xml .= $this->addSitemapEntries('hotel',             $hotelPages);
-        $xml .= $this->addSitemapEntries('repairshops',       $repairShopPages);
-        $xml .= $this->addSitemapEntries('carwashes',         $carWashPages);
-        $xml .= $this->addSitemapEntries('towedvehicle',      $towedVehicleStoragePages);
-        $xml .= $this->addSitemapEntries('parkingfacilities', $parkingFacilityPages); // 공영주차장
-        $xml .= $this->addSitemapEntries('stores',            $storePages);
-        $xml .= $this->addSitemapEntries('evstations',        $evStationPages); // 전기차 충전소
-        $xml .= $this->addSitemapEntries('chargingstations',  $chargingStationPages); // charging_stations
+        $xml .= $this->addSitemapEntries('events',            ceil($totalEvents / $itemsPerPage));
+        $xml .= $this->addSitemapEntries('gasstations',       ceil($totalGasStations / $itemsPerPage));
+        $xml .= $this->addSitemapEntries('parkinglots',       ceil($totalParkingLots / $itemsPerPage));
+        $xml .= $this->addSitemapEntries('hotel',             ceil($totalHotels / $itemsPerPage));
+        $xml .= $this->addSitemapEntries('repairshops',       ceil($totalRepairShops / $itemsPerPage));
+        $xml .= $this->addSitemapEntries('carwashes',         ceil($totalCarWashes / $itemsPerPage));
+        $xml .= $this->addSitemapEntries('towedvehicle',      ceil($totalTowedVehicleStorages / $itemsPerPage));
+        $xml .= $this->addSitemapEntries('parkingfacilities', ceil($totalParkingFacilities / $itemsPerPage));
+        $xml .= $this->addSitemapEntries('stores',            ceil($totalStores / $itemsPerPage));
+        $xml .= $this->addSitemapEntries('evstations',        ceil($totalEvStations / $itemsPerPage));
+        $xml .= $this->addSitemapEntries('chargingstations',  ceil($totalChargingStations / $itemsPerPage));
+
         $xml .= "</sitemapindex>";
 
         return $this->response
-                    ->setHeader('Content-Type', 'application/xml; charset=utf-8')
-                    ->setBody($xml);
+            ->setHeader('Content-Type', 'application/xml; charset=utf-8')
+            ->setBody($xml);
     }
 
     private function addSitemapEntries(string $type, int $pages): string
     {
         $entries = '';
+        $today = date('Y-m-d');
+
         for ($i = 1; $i <= $pages; $i++) {
             $entries .= "<sitemap>\n";
             $entries .= "  <loc>" . base_url("sitemap/{$type}/{$i}") . "</loc>\n";
-            $entries .= "  <lastmod>" . date('Y-m-d') . "</lastmod>\n";
+            $entries .= "  <lastmod>{$today}</lastmod>\n";
             $entries .= "</sitemap>\n";
         }
+
         return $entries;
     }
 
-    public function events(int $pageNumber)
-    {
-        return $this->generateSitemap(
-            'getEventsForSitemap',
-            'events/detail',
-            'created_at',
-            $pageNumber,
-            'daily',
-            '0.8'
-        );
-    }
-
-    public function gasstations(int $pageNumber)
-    {
-        return $this->generateSitemap(
-            'getGasStationsForSitemap',
-            'gas_stations',
-            'data_reference_date',
-            $pageNumber,
-            'daily',
-            '0.7'
-        );
-    }
-
-    public function parkinglots(int $pageNumber)
-    {
-        return $this->generateSitemap(
-            'getParkingLotsForSitemap',
-            'parking/detail',
-            'data_reference_date',
-            $pageNumber,
-            'daily',
-            '0.6'
-        );
-    }
-
-    public function hotel(int $pageNumber)
-    {
-        return $this->generateSitemap(
-            'getHotelsForSitemap',
-            'hotel/detail',
-            'last_update_time',
-            $pageNumber,
-            'daily',
-            '0.9'
-        );
-    }
-
-    public function repairshops(int $pageNumber)
-    {
-        return $this->generateSitemap(
-            'getRepairShopsForSitemap',
-            'automobile_repair_shop',
-            'data_reference_date',
-            $pageNumber,
-            'daily',
-            '0.7'
-        );
-    }
-
-    public function carwashes(int $pageNumber)
-    {
-        return $this->generateSitemap(
-            'getCarWashesForSitemap',
-            'carwash/details',
-            'Data_Reference_Date',
-            $pageNumber,
-            'daily',
-            '0.8'
-        );
-    }
-
-    public function towedvehicle(int $pageNumber)
-    {
-        return $this->generateSitemap(
-            'getTowedVehicleStoragesForSitemap',
-            'towed-vehicle-storage/detail',
-            'data_reference_date',
-            $pageNumber,
-            'daily',
-            '0.8'
-        );
-    }
-
-    public function parkingfacilities(int $pageNumber)
-    {
-        return $this->generateSitemap(
-            'getParkingFacilitiesForSitemap',
-            'parking-facilities',
-            '데이터기준일자',
-            $pageNumber,
-            'daily',
-            '0.8'
-        );
-    }
-
-    public function stores(int $pageNumber)
-    {
-        return $this->generateSitemap(
-            'getStoresForSitemap',
-            'stores',
-            'updated_at',
-            $pageNumber,
-            'daily',
-            '0.8'
-        );
-    }
-
-    // 전기차 충전소 엔드포인트
-    public function evstations(int $pageNumber)
-    {
-        return $this->generateSitemap(
-            'getEvStationsForSitemap', // SitemapModel 메서드
-            'ev-stations',             // 라우트 prefix
-            '',                        // 날짜 필드 없음, fallback to today
-            $pageNumber,
-            'daily',
-            '0.7'
-        );
-    }
-
-    // charging_stations 엔드포인트 추가
-    public function chargingstations(int $pageNumber)
-    {
-        return $this->generateSitemap(
-            'getChargingStationsForSitemap', // SitemapModel 메서드
-            'station/detail',                     // 라우트 prefix
-            '',                                      // 날짜 필드 없음, fallback to today
-            $pageNumber,
-            'daily',
-            '0.7'
-        );
-    }
+    public function events(int $pageNumber)             { return $this->generateSitemap('getEventsForSitemap', 'events/detail', 'created_at', $pageNumber, 'daily'); }
+    public function gasstations(int $pageNumber)        { return $this->generateSitemap('getGasStationsForSitemap', 'gas_stations', 'data_reference_date', $pageNumber, 'daily'); }
+    public function parkinglots(int $pageNumber)        { return $this->generateSitemap('getParkingLotsForSitemap', 'parking/detail', 'data_reference_date', $pageNumber, 'daily'); }
+    public function hotel(int $pageNumber)              { return $this->generateSitemap('getHotelsForSitemap', 'hotel/detail', 'last_update_time', $pageNumber, 'daily'); }
+    public function repairshops(int $pageNumber)        { return $this->generateSitemap('getRepairShopsForSitemap', 'automobile_repair_shop', 'data_reference_date', $pageNumber, 'daily'); }
+    public function carwashes(int $pageNumber)          { return $this->generateSitemap('getCarWashesForSitemap', 'carwash/details', 'Data_Reference_Date', $pageNumber, 'daily'); }
+    public function towedvehicle(int $pageNumber)       { return $this->generateSitemap('getTowedVehicleStoragesForSitemap', 'towed-vehicle-storage/detail', 'data_reference_date', $pageNumber, 'daily'); }
+    public function parkingfacilities(int $pageNumber)  { return $this->generateSitemap('getParkingFacilitiesForSitemap', 'parking-facilities', '데이터기준일자', $pageNumber, 'daily'); }
+    public function stores(int $pageNumber)             { return $this->generateSitemap('getStoresForSitemap', 'stores', 'updated_at', $pageNumber, 'daily'); }
+    public function evstations(int $pageNumber)         { return $this->generateSitemap('getEvStationsForSitemap', 'ev-stations', '', $pageNumber, 'daily'); }
+    public function chargingstations(int $pageNumber)   { return $this->generateSitemap('getChargingStationsForSitemap', 'station/detail', '', $pageNumber, 'daily'); }
 
     private function generateSitemap(
         string $method,
         string $baseRoute,
         string $dateField,
         int    $pageNumber,
-        string $changefreq,
-        string $priority
+        string $changefreq
     ) {
         $sitemapModel = new SitemapModel();
         $itemsPerPage = 10000;
-        $offset       = ($pageNumber - 1) * $itemsPerPage;
-
+        $offset = ($pageNumber - 1) * $itemsPerPage;
         $data = $sitemapModel->$method($itemsPerPage, $offset);
+
+        $today = date('Y-m-d');
 
         $xml  = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
         $xml .= "<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\">\n";
 
         foreach ($data as $item) {
             $idField = isset($item['ID']) ? 'ID' : 'id';
-            $url     = base_url("{$baseRoute}/{$item[$idField]}");
-            $lastMod = date('Y-m-d', strtotime($item[$dateField] ?? date('Y-m-d')));
+            $url = base_url("{$baseRoute}/{$item[$idField]}");
 
-            $xml .= "<url>\n";
-            $xml .= "  <loc>{$url}</loc>\n";
-            $xml .= "  <lastmod>{$lastMod}</lastmod>\n";
-            $xml .= "  <changefreq>{$changefreq}</changefreq>\n";
-            $xml .= "  <priority>{$priority}</priority>\n";
-            $xml .= "</url>\n";
+            $xml .= "  <url>\n";
+            $xml .= "    <loc>{$url}</loc>\n";
+            $xml .= "    <lastmod>{$today}</lastmod>\n";
+            $xml .= "    <changefreq>{$changefreq}</changefreq>\n";
+            $xml .= "  </url>\n";
         }
 
         $xml .= "</urlset>";
 
         return $this->response
-                    ->setHeader('Content-Type', 'application/xml; charset=utf-8')
-                    ->setBody($xml);
+            ->setHeader('Content-Type', 'application/xml; charset=utf-8')
+            ->setBody($xml);
     }
 }
